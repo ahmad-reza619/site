a1b2219d750fcb62a01a1c875fb6ae74bc798764
Ahmad Reza <hungry.dev39@gmail.com>
2020-07-30 20:37:49 +0700

    reset & add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Personal Space of Ahmad Reza</title>
+      </Head>
+      Hello World
+    </div>
+  );
+}
