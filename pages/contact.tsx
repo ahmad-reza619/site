6f3b0358623ce32af4709c84f5690487171c1a2e
Ahmad Reza <hungry.dev39@gmail.com>
2020-10-29 20:14:42 +0700

    fix alignment in contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,16 +12,19 @@ const data = [
     icon: faTwitter,
     title: 'twitter'.toUpperCase(),
     desc: 'I usually tweet or retweet about tech here',
+    links: 'https://twitter.com/HungryDev1',
   },
   {
     icon: faGithub,
     title: 'github'.toUpperCase(),
     desc: 'I do open souce sometimes, follow me please :)',
+    links: 'https://github.com/ahmad-reza619',
   },
   {
     icon: faLinkedin,
     title: 'linkedin'.toUpperCase(),
     desc: 'Wanted to connect with me in linkedin?',
+    links: 'https://www.linkedin.com/in/ahmad-reza-68ab52170/',
   },
 ]
 
@@ -30,19 +33,22 @@ export default function Contact(){
     <Layout>
       <div className={classes.container}>
         <Text as="h1">Contact Me</Text>
-        {data.map((d) => (
-          <div className={classes.contactLinkCard} key={d.title}>
-            <Text className={classes.iconWrapper}>
-              <FontAwesomeIcon icon={d.icon} size="6x" className={classes.icon} />
-            </Text>
-            <div className={classes.contactLinkCardContent}>
-              <Text as="h5">{d.title}</Text>
-              <Text as="p">
-                {d.desc}
+        <div className={classes.contactLinks}>
+          {data.map((d) => (
+            <React.Fragment>
+              <Text>
+                <FontAwesomeIcon icon={d.icon} size="6x" />
               </Text>
-            </div>
-          </div>
-        ))}
+              <a href={d.links} className={classes.contactLinkCardContent}>
+                <Text as="h5">{d.title}</Text>
+                <Text as="p">
+                  {d.desc}
+                </Text>
+              </a>
+            </React.Fragment>
+          ))}
+        </div>
+        
       </div>
     </Layout>
   )
